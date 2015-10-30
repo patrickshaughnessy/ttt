69f9bfc4b8f7439a83858410f7774137e9439081
Patrick Shaughnessy <pshaughn86@gmail.com>
2015-10-29 19:22:17 -0700

    Add end of game winning display

--- script.js
+++ script.js
@@ -7,8 +7,13 @@
     let finalX = '';
     let finalO = '';
     let winningStates = ['123', '456', '789', '147', '258', '369', '159', '357'];
+    let endGame = false;
 
     $('.box').on('click.mark', (event) => {
+
+      if (endGame){
+        return;
+      }
 
       let $e = $(event.target);
 
@@ -34,11 +39,16 @@
       // if (finalO.length === 5 || finalX.length === 5){
         //check X
         // finalX = finalX.split("").sort().join('');
+
         for (var i = 0, e =3; e <= finalX.length; i++, e++){
           var sliceOfThree = finalX.slice(i,e);
           winningStates.forEach(function(elem){
             if (sliceOfThree == elem) {
-              console.log('X wins!');
+              let winMessageX = '<h1>X wins!</h1>';
+
+              $('#winning').append(winMessageX);
+
+              endGame = true;
               return;
             }
           });
@@ -50,7 +60,12 @@
           var sliceOfThree = finalO.slice(i,e);
           winningStates.forEach(function(elem){
             if (sliceOfThree == elem) {
-              console.log('O wins!');
+              // console.log('O wins!');
+              let winMessageO = '<h1>O wins!</h1>';
+
+              $('#winning').append(winMessageO);
+
+              endGame = true;
               return;
             }
           });
